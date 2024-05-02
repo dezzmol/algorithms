0ebe0f2447f5f8b7bc3c205f63ee285c63e20f06
Radmir <musabaevradmir@gmail.com>
2024-05-02 10:09:27 +0400

    commit

---
+++ src/leetcode/MergeTwoSortedLists21.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class MergeTwoSortedLists21 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Solution solution = new Solution();
+        //System.out.println(solution.mergeTwoLists());
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            if (list1 != null && list2 != null) {
+                if (list1.val < list2.val) {
+                    list1.next = mergeTwoLists(list1.next, list2);
+                    return list1;
+                } else {
+                    list2.next = mergeTwoLists(list1, list2.next);
+                    return list2;
+                }
+            }
+            if (list1 == null) {
+                return list2;
+            }
+            return list1;
+        }
+    }
+}
+
+
