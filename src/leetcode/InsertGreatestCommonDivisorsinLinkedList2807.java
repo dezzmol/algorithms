32290a9d2f49fc7eb77411e31e819a151561a1d7
Radmir <musabaevradmir@gmail.com>
2024-05-22 11:37:54 +0400

    commit

---
+++ src/leetcode/InsertGreatestCommonDivisorsinLinkedList2807.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+public class InsertGreatestCommonDivisorsinLinkedList2807 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode listNode = head;
+        while (listNode.next.next != null) {
+            listNode.next = new ListNode(gcd(listNode.val, listNode.next.val), listNode.next);
+            listNode = listNode.next.next;
+        }
+        return head;
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
