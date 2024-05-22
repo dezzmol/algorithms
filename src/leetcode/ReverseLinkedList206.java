1d22a6ff19ebbaed03afea72928013483d5ecdfc
Radmir <musabaevradmir@gmail.com>
2024-05-22 12:04:02 +0400

    commit

---
+++ src/leetcode/ReverseLinkedList206.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+
+
+public class ReverseLinkedList206 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null;
+
+        while (temp.next != null) {
+            ListNode next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+
+        return prev;
+    }
+}
