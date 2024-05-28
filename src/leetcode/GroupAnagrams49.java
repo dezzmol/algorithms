90eba2abe78bbb889bd5bc35abf4605317125953
Radmir <musabaevradmir@gmail.com>
2024-05-29 02:16:57 +0400

    commit

---
+++ src/leetcode/GroupAnagrams49.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.*;
+
+public class GroupAnagrams49 {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>(strs.length);
+        Map<String, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < strs.length; i++) {
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String string = new String(chars);
+            if (map.containsKey(string)) {
+                List<Integer> list = map.get(string);
+                list.add(i);
+                map.put(string, list);
+            } else {
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                map.put(string, list);
+            }
+        }
+
+        for (List<Integer> indices : map.values()) {
+            List<String> group = new ArrayList<>();
+            for (int index : indices) {
+                group.add(strs[index]);
+            }
+            result.add(group);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
