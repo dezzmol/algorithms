e856fd405bded0fb57052b16f2f9ed9c86717306
Radmir Musabaev <musabaevradmir@gmail.com>
2024-08-03 14:00:31 +0400

    done leetcode 938

---
+++ src/leetcode/L938_RangeSumofBST.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import leetcode.utils.TreeNode;
+
+public class L938_RangeSumofBST {
+    public static void main(String[] args) {
+
+    }
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+}
