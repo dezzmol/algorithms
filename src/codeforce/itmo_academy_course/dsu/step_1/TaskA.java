ab099df524054d96859c567f375cc4a015338182
Radmir Musabaev <musabaevradmir@gmail.com>
2024-08-09 21:01:41 +0400

    done itmo academy course dsu taskA

--- src/codeforce/itmo_academy_course/dsu/step_1/TaskA.java
+++ src/codeforce/itmo_academy_course/dsu/step_1/TaskA.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 public class TaskA {
     static int[] p;
     static int[] r;
-    static int[] min;
-    static int[] max;
-    static int[] size;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -16,15 +13,9 @@ public class TaskA {
         int m = scanner.nextInt();
         p = new int[n + 1];
         r = new int[n + 1];
-        min = new int[n + 1];
-        max = new int[n + 1];
-        size = new int[n + 1];
         for (int i = 1; i < p.length; i++) {
             p[i] = i;
             r[i] = 0;
-            min[i] = i;
-            max[i] = i;
-            size[i] = 1;
         }
 
         for (int i = 0; i < m; i++) {
@@ -38,9 +29,12 @@ public class TaskA {
             }
             if (command.equals("get")) {
                 int a = scanner.nextInt();
-                System.out.print(getMin(a) + " ");
-                System.out.print(getMax(a) + " ");
-                System.out.println(getSize(a));
+                int b = scanner.nextInt();
+                if (get(a) == get(b)) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
             }
         }
 
@@ -55,40 +49,12 @@ public class TaskA {
         }
         if (r[a] > r[b]) {
             p[b] = a;
-            min[b] = Math.min(min[b], min[a]);
-            setMax(b, Math.max(max[a], max[b]));
-            size[b] += size[a];
         } else {
             p[a] = b;
-            min[a] = Math.min(min[b], min[a]);
-            setMax(a, Math.max(max[a], max[b]));
-            size[a] += size[b];
         }
     }
 
     private static int get(int a) {
-        if (p[a] != a) {
-            p[a] = get(p[a]);
-        }
-        return p[a];
-    }
-
-    private static int getMin(int a) {
-        return min[get(a)];
-    }
-
-    private static int setMax(int a, int m) {
-        if (max[a] != m) {
-            max[a] = setMax(max[a], m);
-        }
-        return max[a];
-    }
-
-    private static int getMax(int a) {
-        return max[get(a)];
-    }
-
-    private static int getSize(int a) {
-        return size[get(a)];
+        return p[a] = (p[a] == a ? a : get(p[a]));
     }
 }
