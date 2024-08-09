be39e8a88926bb21a6557abe052d3e1d2b2c5c05
Radmir <musabaevradmir@gmail.com>
2024-08-09 12:55:34 +0400

    done leetcode 19

--- src/leetcode/L19_RemoveNthNodeFromEndofList.java
+++ src/leetcode/L19_RemoveNthNodeFromEndofList.java
@@ -1,59 +1,35 @@
 package leetcode;
 
+import leetcode.utils.ListNode;
+
 public class L19_RemoveNthNodeFromEndofList {
-    static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-
-        @Override
-        public String toString() {
-            return "ListNode{" +
-                    "val=" + val +
-                    ", next=" + next +
-                    '}';
-        }
-    }
 
     public static ListNode removeNthFromEnd(ListNode head, int n) {
-        int nodeCount = 0;
-        ListNode node = head;
-        while (node != null) {
-            node = node.next;
-            nodeCount++;
+        ListNode temp = new ListNode(-1, head);
+        ListNode fast = head;
+
+        for (int i = 0; i < n - 1; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            temp = temp.next;
+            fast = fast.next;
         }
 
-        if (nodeCount == 1 && n == 1) {
-            return null;
+        if (temp.next == head) {
+            head = temp.next.next;
         }
+        temp.next = temp.next.next;
 
-        int nodeForRemove = nodeCount - n - 1;
-        node = head;
-        int nodeIndex = 0;
-        while (nodeIndex < nodeCount && node != null && node.next != null) {
-            if (nodeIndex == nodeForRemove) {
-                node.next = node.next.next;
-            }
-            node = node.next;
-            nodeIndex++;
-        }
         return head;
     }
 
     public static void main(String[] args) {
         ListNode node1 = new ListNode(1);
         node1.next = new ListNode(2);
+        node1.next.next = new ListNode(3);
+        node1.next.next.next = new ListNode(4);
+        node1.next.next.next.next = new ListNode(5);
 
         System.out.println(removeNthFromEnd(node1, 2));
     }
