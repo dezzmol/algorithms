cb79c53d25a6184a678b1f62497dacd3d214c404
Radmir Musabaev <musabaevradmir@gmail.com>
2024-08-11 19:54:02 +0400

    done itmo academy course bin search taskC

---
+++ src/codeforce/itmo_academy_course/bin_search/step_1/TaskC.java
@@ -0,0 +1,41 @@
+package codeforce.itmo_academy_course.bin_search.step_1;
+
+import java.util.Scanner;
+
+public class TaskC {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        int k = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < k; i++) {
+            int x = scanner.nextInt();
+            System.out.println(bin(arr, x));
+        }
+    }
+
+    private static int bin(int[] arr, int x) {
+        int l = -1, r = arr.length;
+
+        while (r > l + 1) {
+            int mid = (l + r) / 2;
+
+            if (arr[mid] < x) {
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+
+        return r + 1;
+    }
+
+
+}
