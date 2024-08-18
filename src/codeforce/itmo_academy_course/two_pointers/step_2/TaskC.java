18b9d0012948754bb9cb51fac05ba04d9a3dcaea
Radmir Musabaev <musabaevradmir@gmail.com>
2024-08-18 13:08:14 +0400

    done itmo academy course two pointers step 2 taskC

---
+++ src/codeforce/itmo_academy_course/two_pointers/step_2/TaskC.java
@@ -0,0 +1,30 @@
+package codeforce.itmo_academy_course.two_pointers.step_2;
+
+import java.util.Scanner;
+
+public class TaskC {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        long s = scanner.nextLong();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        long sum = 0;
+        int l = 0;
+        long res = 0;
+        for (int r = 0; r < n; r++) {
+            sum += a[r];
+            while (sum > s) {
+                sum -= a[l];
+                l++;
+            }
+            res += r - l + 1;
+        }
+
+        System.out.println(res);
+    }
+}
